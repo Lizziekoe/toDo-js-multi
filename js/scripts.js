69ff00157b11de79797c6404b1079e67d1694a73
Lizzie Koehler and Tyler Brown <elementalplug@gmail.com>
2015-02-19 12:25:07 -0800

    Add a prototype List and a click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
-$(function(){
+var List = { name: ""
+             }
+
+$(function() {
+  $("#listForm").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("#listName").val();
+    var newList = Object.create(List);
+    newList.name = inputtedName;
+
+    $("#lists").append("<li>" + newList.name + "</li>");
+
+    $("#listName").val("");
+
+
+    $("#lists").click(function() {
+      $("#listTitle h2").text(newList.name);
+      debugger;
+
+    });
+  });
+
   $("#toDoForm").submit(function(event){
 
     var inputTask = $("#task").val();
-    inputTask = inputTask[0].toUpperCase() + inputTask.slice(1);  
+    inputTask = inputTask[0].toUpperCase() + inputTask.slice(1);
 
-    if(inputTask){
+    if (inputTask) {
       var task = {description1: inputTask};
       $("#toDoList").append("<li><span class='toDo'>" + task.description1 + "</span></li>");
     } else{
