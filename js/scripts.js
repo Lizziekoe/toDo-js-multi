fdd9df320b21421a6341798b7ee14e4f5de194d4
Lizzie Koehler and Tyler Brown <elementalplug@gmail.com>
2015-02-19 15:47:52 -0800

    Adds lots of comments to code for fellow students

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var Task = { description1: ""}
 
 $(function() {
   var currentList = null;
+  // This is being used as a global variable to be reassigned as needed in between the two submit events
 
   $("#listForm").submit(function(event) {
     event.preventDefault();
@@ -11,6 +12,8 @@ $(function() {
     var newList = Object.create(List);
     newList.name = inputtedName;
     newList.tasks = [];
+    // This puts in a new array into the newList so that it is a unique array, rather than having
+    // a prototype with a 'tasks' array that deals with lots of inheritance / prototypal issues
 
     $("#lists").append("<li><span class='listItem'>" + newList.name + "</span></li>");
 
@@ -19,11 +22,15 @@ $(function() {
     $(".listItem").last().click(function() {
       $("#listTitle h2").text(newList.name);
       currentList = newList
+      // This is reassigning the newList properties previously handled to the currentList,
+      // which can now be manipulated
       $('#toDoList').empty();
+      // When a new list is clicked, the .empty function clears out the tasks currently displayed
       currentList.tasks.forEach(function(task) {
-
         $("#toDoList").append("<li><span class='toDo'>" + task.description1 + "</span></li>");
-
+      // This loop, which is forEach instead of each because it is dealing with the array of tasks,
+      // is repopulating the list of tasks to be done unique to the newly clicked on list after the
+      // tasks list has been emptied
       });
     });
   });
@@ -44,6 +51,8 @@ $(function() {
     } else {
       alert("Please input a task")
     }
+    // This if statement worked in the previous iteration of the script, but now is slightly broken.
+    // We didn't remove it because it breaks the true functionality of the document... we think.
 
     $(".toDo").last().click(function(){
       $("#doneList").append("<li><span class='done'>" + newTask.description1 + "</span></li>");
