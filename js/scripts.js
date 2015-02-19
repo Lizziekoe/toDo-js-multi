dc3c20696473c9f773f57573982e9d8cb68f87b3
Lizzie Koehler and Tyler Brown <elementalplug@gmail.com>
2015-02-19 14:57:07 -0800

    link lists to tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
-var List = { name: ""
-             }
+var List = { name: "" }
+var Task = { description1: ""}
 
 $(function() {
+  var currentList = null;
+
   $("#listForm").submit(function(event) {
     event.preventDefault();
 
     var inputtedName = $("#listName").val();
     var newList = Object.create(List);
     newList.name = inputtedName;
+    newList.tasks = [];
 
-    $("#lists").append("<li>" + newList.name + "</li>");
+    $("#lists").append("<li><span class='listItem'>" + newList.name + "</span></li>");
 
     $("#listName").val("");
 
-
-    $("#lists").click(function() {
+    $(".listItem").last().click(function() {
       $("#listTitle h2").text(newList.name);
-      debugger;
+      currentList = newList
+      $('#toDoList').empty();
+      currentList.tasks.forEach(function(task) {
 
+        $("#toDoList").append("<li><span class='toDo'>" + task.description1 + "</span></li>");
+
+      });
     });
   });
 
@@ -26,23 +33,24 @@ $(function() {
     var inputTask = $("#task").val();
     inputTask = inputTask[0].toUpperCase() + inputTask.slice(1);
 
-    if (inputTask) {
-      var task = {description1: inputTask};
-      $("#toDoList").append("<li><span class='toDo'>" + task.description1 + "</span></li>");
-    } else{
+    if (inputTask !== "") {
+      var newTask = Object.create(Task);
+      newTask.description1 = inputTask;
+
+      currentList.tasks.push(newTask);
+
+      $("#toDoList").append("<li><span class='toDo'>" + newTask.description1 + "</span></li>");
+
+    } else {
       alert("Please input a task")
     }
 
     $(".toDo").last().click(function(){
-      $("#doneList").append("<li><span class='done'>" + task.description1 + "</span></li>");
+      $("#doneList").append("<li><span class='done'>" + newTask.description1 + "</span></li>");
       $(this).parent('li').remove();
     });
 
     $("#task").val("");
-
     event.preventDefault();
   });
-
-
-
 });
